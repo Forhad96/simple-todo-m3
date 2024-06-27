751fe3bfd1532d161104aa765eaf815d87ffb460
Forhad96 <forhadairdrop@gmail.com>
2024-06-27 15:43:39 +0600

    Add:todo state toggle functionality done

--- src/redux/features/todoSlice.ts
+++ src/redux/features/todoSlice.ts
@@ -20,12 +20,18 @@ const todoSlice = createSlice({
     addTodo: (state, actions: PayloadAction<TTodo>) => {
       state.todos.push({ ...actions.payload, isCompleted: false });
     },
-    removeTodo:(state,actions:PayloadAction<string>)=>{
-       state.todos = state.todos.filter(item => item.id !== actions.payload)
-    }
+    removeTodo: (state, actions: PayloadAction<string>) => {
+      state.todos = state.todos.filter((item) => item.id !== actions.payload);
+    },
+    toggleTodoState: (state, actions: PayloadAction<string>) => {
+      const todo = state.todos.find((item) => item.id === actions.payload);
+      if (todo) {
+        todo.isCompleted = !todo.isCompleted;
+      }
+    },
   },
 });
 
-export const { addTodo,removeTodo } = todoSlice.actions;
+export const { addTodo, removeTodo,toggleTodoState } = todoSlice.actions;
 
 export default todoSlice.reducer;
